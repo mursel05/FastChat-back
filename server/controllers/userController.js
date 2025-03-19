56ec6d1ffe105d697ba839549f3345efa9542c55
Mursel <mursal.haxverdiyev@gmail.com>
2025-03-20 03:45:23 +0400

    cookies bug fixed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -64,6 +64,8 @@ exports.login = async (req, res) => {
         if (tokens) {
           res.cookie("refreshToken", tokens.refreshToken, {
             httpOnly: true,
+            secure: true,
+            sameSite: "none",
             maxAge: process.env.REFRESH_TOKEN_EXPIRES_IN * 1000,
           });
           res.cookie("accessToken", tokens.accessToken, {
